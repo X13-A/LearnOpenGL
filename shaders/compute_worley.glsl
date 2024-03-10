0c3b0f75db16f6291fee685e6f9938e9a6d307aa
Alex <foulonalex@yahoo.com>
2024-03-10 23:47:30 +0100

    Optimize worley points data

--- shaders/compute_worley.glsl
+++ shaders/compute_worley.glsl
@@ -4,7 +4,7 @@ layout(local_size_x = 4, local_size_y = 4, local_size_z = 4) in;
 
 layout(std430, binding = 0) buffer PointBuffer 
 {
-    vec4 points[];
+    float points[];
 };
 
 layout(binding = 1, rgba8) uniform image3D outputTexture;
@@ -14,33 +14,21 @@ uniform int pointsBufferLength;
 void main() 
 {
     ivec3 texelPos = ivec3(gl_GlobalInvocationID.xyz);
-
     vec3 normalizedPos = vec3(texelPos) / vec3(textureDimensions);
+
     float minDist = 10000000;
-    for (int i = 0; i < pointsBufferLength; ++i)
+    for (int i = 0; i < pointsBufferLength;)
     {
-        float newDist = distance(normalizedPos, points[i].xyz);
+        vec3 currentPoint = vec3(points[i++], points[i++], points[i++]);
+        float newDist = distance(normalizedPos, currentPoint);
         if (newDist < minDist)
         {
             minDist = newDist;
         }
     }
 
-    //if (minDist < 0.05)
-    //{
-    //    imageStore(outputTexture, texelPos, vec4(1, 1, 1, 1));
-    //    return;
-    //}
-    //else
-    //{
-    //    imageStore(outputTexture, texelPos, vec4(0, 0, 0, 0));
-    //    return;
-    //}
-
     float worleyDiminutionFactor = 10;
-    float worleyIntensityFactor = 2;
-    float worleyDist = (1 - minDist * worleyDiminutionFactor) * worleyIntensityFactor;
-    
-    vec4 color = vec4(worleyDist, worleyDist, worleyDist, 1);
+    float worleyDist = (1 - minDist * worleyDiminutionFactor);
+    vec4 color = vec4(vec3(worleyDist), 1);
     imageStore(outputTexture, texelPos, color);
 }
